06946c9ec414e3575329f76a7836ff430778a9fe
Javi590 <77155899+Javi590@users.noreply.github.com>
2021-01-11 09:53:11 +0100

    Notas sobre Scripts y Markdown

---
+++ scripts/00-test.R
@@ -0,0 +1,14 @@
+> * Script * : para incluir instrucciones. Se pueden ejecutar. Permite guardar:
+  variables de instrucciones
+  funciones
+  datos de prueba
+  comentar cosas que no han funcionado
+
+lm(Y~x)
+  No sabemos por qué no funciona. Ponemos Hashtag delante y quedará comentado:
+  # Esta línea no funciona, por ahora...
+      # lm(Y~x)
+  Para ejecutar un Script tecla Alt+Enter o el botón run (se ve en la zona de la consola)
+  #Hay que acostumbrarse a comentar el código.
+  
+* RMD o R Markdown * : Para informes
