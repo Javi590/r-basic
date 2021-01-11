948ad7933721b498397eb783ef3b89c41cc32a64
Javi590 <77155899+Javi590@users.noreply.github.com>
2021-01-11 12:27:34 +0100

    se guardan cambios de logaritmos

--- 04-calculadora.R
+++ 04-calculadora.R
@@ -1,1 +1,76 @@
 2*(3+5/2)
+
+2*((3+5)/2)
+
+2/3+5
+
+2%/%3+5
+
+2%%3
+
+2^3*5
+
+2^(-4)
+
+Cuantas semanas son 725 dias.
+
+725%/%7
+
+725%%7
+
+#D= d *q +r
+#r_ D-d*q
+#q= D %/% d
+#r= D %% d
+725-103*7
+
+pi
+
+#Tau
+  2*pi
+  #Ojo con el simbolo. Si ponemos 2pi:
+  2pi
+  #Error: unexpected symbol in "2pi"
+3^pi
+
+pi^2
+
+Inf
+
+-Inf
+
+#Not a number: no hay forma de calcularlo, no existe número para representarlo:
+
+  NaN
+
+5/0
+
+0/0
+
+2^50 # = 1.1259e+15 = 1125900000000000
+
+2^(-15) # = 3.051758e-05 = 0.00003051758
+
+#Hay que entenderlo como notacion cientifica.
+
+#◘Vectores: a pesar de haber enteros, eleva a 0. Cuando un numero esta rodeado de numeros en notacion cientifica se le aplica esta.
+
+c(2^30, 2^(-15), 1, 2, 3/2)
+
+#Otras funciones:
+
+#Raiz cuadrada
+sqrt(5)
+
+#logaritmo de la exponencial de 1. Cuya exponencial es el numero e:
+log(exp(1))
+
+#logaritmo neperiano y exponencial son funciones opuestas.
+
+#logaritmo de 1000 en base 10. 10 elevado a 3 da 1000.
+
+log(1000,10)
+
+#Logaritmo en base 10 de 1000 (igual que el anterior)
+log10(1000)
+
