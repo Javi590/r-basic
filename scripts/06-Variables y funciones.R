1f1ffd97354dfcb1d3ec1d91b241fb8ba6dd3311
Javi590 <77155899+Javi590@users.noreply.github.com>
2021-01-12 14:11:42 +0100

    Variables y funciones

---
+++ scripts/06-Variables y funciones.R
@@ -0,0 +1,44 @@
+# VARIABLES Y FUNCIONES
+
+#R funciona con objetos (estructuras de distintos tipos que se utilizan para hacer tareas)
+
+#Una VARIABLE es un objeto que sirve para guardar datos (información). Para crearla hay que dar un nombre a la variable, escribir un símbolo igual (=) o una flecha (<- o -> en función de dónde se encuentre la variable y el valor), y luego el valor que le queremos dar.
+
+x= (pi^2)/2
+x
+
+y<-cos(pi/4)
+y
+
+sin(pi/4) + cos (pi/4) -> z
+z
+
+#Las variables creadas se van guardando en la pestaña de entorno. Se puede ver en lista o parrilla, en parrilla te dan más información sobre ella. Si ejecutas el nombre de la variable (p.ej. x o y) te devuelve el número asociado a ella.
+
+
+# También se puede utilizar cualquier palabra como nombre de variable, poniéndolo entre comas "valor":
+edad <- 30
+nombre = "Javier"
+
+#R distingue entre mayúsculas y minúsculas. Recomiendan no usar caracteres linguisticos extraños (diéresis, acentos...)
+
+pi.4 =4*pi
+
+#♦ Las variables pueden cambiar su valor:
+x=2
+
+x=x^2 # Coge el valor de la variable guardada (4)
+
+x=x^2 # (16)
+
+# Ejercicios:
+
+nombre= "Javier Trujillo"
+x=4
+y=2
+z=15
+edad= 25 (#No sé borrar una variable del entorno)
+  Edad=0
+edad=30
+sexo= "hombre"
+Estado.civil= "soltero"
